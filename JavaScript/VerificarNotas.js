4ac7acc88b798feda5f42b66909cd6b9323bbb40
Álvaro José <josealvaro3421@gmail.com>
2024-02-29 22:00:03 -0300

    atualizações dos problemas ainda não funfando 100%

--- JavaScript/VerificarNotas.js
+++ JavaScript/VerificarNotas.js
@@ -7,18 +7,14 @@ function VerificarNotas() {
     for (let i = 0; i < iteradorLinhasTabela; i++) {
 
         // COMPUTAÇÃO DOS VALORES RECEBIDOS NOS INPUTS HTML
-        var valorNota01 = parseInt(document.getElementById("entrada_nota01_" + i).value)
-        var valorNota02 = parseInt(document.getElementById("entrada_nota02_" + i).value)
-        var valorNota03 = parseInt(document.getElementById("entrada_nota03_" + i).value)
-        var valorNota04 = parseInt(document.getElementById("entrada_nota04_" + i).value)
+        var valorNota01 = parseInt(document.getElementById(`entrada_nota01_${i}`).value)
+        var valorNota02 = parseInt(document.getElementById(`entrada_nota02_${i}`).value)
+        var valorNota03 = parseInt(document.getElementById(`entrada_nota03_${i}`).value)
+        var valorNota04 = parseInt(document.getElementById(`entrada_nota04_${i}`).value)
 
         // CAULCULO DA MÉDIA DO ALUNO
         let soma = valorNota01 + valorNota02 + valorNota03 + valorNota04
         var media = soma / 4
-
-        // CRIAÇÃO DO ATRIBUTO PARA OUTPUT (SITUAÇÃO) E ATRIBUIÇÃO DE SEU VALOR
-        var atributoSaidaSituacao = document.createAttribute("id")
-        atributoSaidaSituacao.value = "saida_situacao_" + i
 
         if (media > 50) {
             var textoSaidaSituacao = document.createTextNode("APROVADO")
@@ -30,10 +26,9 @@ function VerificarNotas() {
             var textoSaidaSituacao = document.createTextNode("RECUPERADO")
         }
 
+
         // CRIAÇÃO DO ATRIBUTO PARA OUTPUT (MÉDIA) E ATRIBUIÇÃO DE SEU VALOR
-        var atributoSaidaMedia   = document.createAttribute("id")
-        var textoSaidaMedia      = document.createTextNode(media)
-        atributoSaidaMedia.value = "saida_media_" + i
+        var textoSaidaMedia = document.createTextNode(media)
 
         // INDENTIFICAÇÃO DA LINHA DO ALUNO SENDO ITERADA
         var linhaAluno = document.getElementById("linha_aluno_" + i)
@@ -47,17 +42,17 @@ function VerificarNotas() {
         var outputMedia = document.createElement("output")
         
         // CRIAÇÃO DE LAÇOS PAI E FILHO PARA SITUAÇÃO
-        outputSituacao.appendChild(atributoSaidaSituacao) // ((output -> atributo)
+        outputSituacao.setAttribute("id", ("saida_situacao_" + i)) // ((output -> atributo)
         outputSituacao.appendChild(textoSaidaSituacao) // (output -> situação)
         colunaSituacao.appendChild(outputSituacao) // (coluna -> output)
         
         // CRIAÇÃO DE LAÇOS PAI E FILHO PARA MÉDIA
-        outputMedia.appendChild(atributoSaidaMedia) // ((output -> atributo)
-        outputMedia.appendChild(textoSaidaMedia) // (output -> situação)
+        outputMedia.setAttribute("id", ("saida_media_" + i)) // ((output -> atributo)
+        outputMedia.innerHTML = media // (output -> situação)
         colunaMedia.appendChild(outputMedia) // (coluna -> output)
         
         //CRIAÇÃO DE LAÇOS PAI E FILHO
-        linhaAluno.appendChild(colunaMedia) // (linha -> coluna)
+        // linhaAluno.appendChild(colunaMedia) // (linha -> coluna)
         linhaAluno.appendChild(colunaSituacao) // (linha -> coluna)
 
         document.body.appendChild(linhaAluno) // (linha -> tabela)
